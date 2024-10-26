d28ac58cb82c510912b5a883e76b79d15f77c900
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2024-10-27 00:41:51 +0100

    tweak config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/me',
   output: 'export',
-
   images: {
     unoptimized: true, // Required for static export since Next.js image optimization requires a server
   },
