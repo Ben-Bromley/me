5b724f9e89e661e2fb328709d4a267a06559fa08
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2024-10-26 23:51:40 +0100

    content updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
 	return (
-		<main className="flex items-center justify-center h-screen">
+		<main className="flex flex-col items-center justify-center h-screen text-center px-4">
+			<div className="mb-24">
+				<h1 className="text-4xl sm:text-7xl mb-4">BEN BROMLEY</h1>
+				<h2 className="text-center text-md sm:text-xl">Software Engineer • Musician • AV Technician</h2>
+			</div>
 			<div>
-				<h1 className="text-7xl mb-4">BEN BROMLEY</h1>
-				<h2 className="text-center text-xl">Software Engineer • Musician • AV Technician</h2>
+				<p>More coming soon...</p>
 			</div>
 		</main>
 	);
