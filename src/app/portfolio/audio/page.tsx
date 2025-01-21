d585178a91728275c16bd05d7d343f13030c7324
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2025-01-21 17:00:56 +0000

    add music and audio portfolio pages

---
+++ src/app/portfolio/audio/page.tsx
@@ -0,0 +1,127 @@
+import { type NextPage } from 'next';
+import Image from 'next/image';
+import mixing from '@/public/images/mixing.jpg';
+
+const AudioPortfolio: NextPage = () => {
+  return (
+    <div>
+      <header className="relative">
+        <Image
+          src={mixing}
+          className="max-h-80 w-full object-cover opacity-70 blur"
+          alt="A glowing mixing desk"
+        />
+        <h1 className="absolute top-1/2 my-4 w-full text-center text-4xl sm:text-7xl">
+          Technical Portfolio
+        </h1>
+      </header>
+      <main className="px-4 py-16">
+        <section className="mx-auto mb-16 max-w-prose">
+          <h2 className="mb-8 text-2xl sm:text-4xl">Introduction</h2>
+          <p className="mb-4">
+            I'm a Music & Music Technology student at the University Of
+            Nottingham. I work in Logic Pro to record, create, and mix music. I
+            have also worked with Cubase for music recording and production. I
+            also work with EOS Family software for lighting design, and QLab for
+            sound design.
+          </p>
+          <p className="mb-4">
+            I'm currently volunteering as the Company Technical Director at the
+            Nottingham New Theatre where I am responsible for the technical
+            training of our lighting and sound designers in QLab, EOS, and
+            rigging of smart lighting fixtures. I also run live sound for our
+            social events such as our "Green Room" & "Drag Night" performance
+            fundraisers, and our Oscars-inspired awards evening.
+          </p>
+        </section>
+        <section className="mx-auto mb-16 max-w-prose">
+          <h2 className="my-8 text-2xl sm:text-4xl">Experience</h2>
+          <div id="experience-table-container" className="relative">
+            <div className="overflow-x-scroll">
+              <table className="w-full min-w-max">
+                <thead>
+                  <tr className="border-b border-gray-700 text-left">
+                    <th className="pb-4">Role</th>
+                    <th>Show</th>
+                    <th className="hidden md:table-cell">Location</th>
+                    <th className="hidden sm:table-cell">Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* <ExperienceTableRow
+                    part="Technical Assistant"
+                    show="Misc. Live Events"
+                    location="TEC PA & Lighting"
+                    date="2024 - Present"
+                  /> */}
+                  <ExperienceTableRow
+                    part="Company Technical Director"
+                    show="All Plays and Events"
+                    location="Nottingham New Theatre"
+                    date="2024 - Present"
+                  />
+                  <ExperienceTableRow
+                    part="Technical Director"
+                    show="Immanuel Kant (Play)"
+                    location="Edinburgh"
+                    date="August 2024"
+                  />
+                  <ExperienceTableRow
+                    part="Lighting Designer"
+                    show="About My Depression (Play)"
+                    location="Nottingham"
+                    date="November 2023"
+                  />
+                  <ExperienceTableRow
+                    part="Lighting Designer"
+                    show="Jack And The Beanstalk (Pantomime)"
+                    location="Leicester"
+                    date="December 2018"
+                  />
+                  <ExperienceTableRow
+                    part="Student Venue Technician"
+                    show="All Showcases"
+                    location="Leicester"
+                    date="2017 - 19"
+                  />
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </section>
+        <section className="mx-auto max-w-prose">
+          <h2 className="mb-8 text-2xl sm:text-4xl">Aspirations</h2>
+          <p className="mb-4">
+            I am looking to expand my portfolio of work in sound design, audio
+            engineering, and lighting design.
+          </p>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default AudioPortfolio;
+
+type ExperienceTableRowProps = {
+  part: string;
+  show: string;
+  location: string;
+  date: string;
+};
+
+const ExperienceTableRow: React.FC<ExperienceTableRowProps> = ({
+  part,
+  show,
+  location,
+  date,
+}) => {
+  return (
+    <tr className="border-b border-gray-700">
+      <td className="max-w-60 py-2 pr-8">{part}</td>
+      <td className="max-w-60 py-2 pr-8">{show}</td>
+      <td className="hidden max-w-60 py-2 pr-8 md:table-cell">{location}</td>
+      <td className="hidden max-w-60 py-2 pr-8 sm:table-cell">{date}</td>
+    </tr>
+  );
+};
---
+++ src/app/portfolio/music/page.tsx
@@ -0,0 +1,132 @@
+import { type NextPage } from 'next';
+import Image from 'next/image';
+import piano from '@/public/images/piano.jpg';
+
+const MusicPortfolio: NextPage = () => {
+  return (
+    <div>
+      <header className="relative">
+        <Image
+          src={piano}
+          className="max-h-80 object-cover opacity-70 blur w-full"
+          alt="A Nord stage piano"
+        />
+        <h1 className="absolute top-1/2 my-4 w-full text-center text-4xl sm:text-7xl">
+          Music Portfolio
+        </h1>
+      </header>
+      <main className="mx-auto max-w-prose px-4 py-16">
+        <section className="mb-16">
+          <h2 className="mb-8 text-2xl sm:text-4xl">Piano</h2>
+          <p className="mb-4">
+            I'm a self taught pianist with 10+ years of performance experience
+            across religious, orchestral, and education settings. I began as a
+            church pianist and organist. I now play in musical theatre pit
+            bands.
+          </p>
+          <p className="mb-4">
+            I am experienced in programming and operating mainstage. I can also
+            design custom patches from scratch using the ES-series synthesiser
+            plugins.
+          </p>
+        </section>
+        <h2 className="my-8 text-2xl sm:text-4xl">Experience</h2>
+        <section className="mb-16">
+          <table className="w-full border-separate">
+            <thead>
+              <tr className="text-left">
+                <th>Part</th>
+                <th>Show</th>
+                <th className='hidden md:table-cell'>Location</th>
+                <th className='hidden sm:table-cell'>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* <ExperienceTableRow
+                part="Keyboard"
+                show="Evita"
+                location="Loughborough, England"
+                date="Sep 2025"
+              /> */}
+              <ExperienceTableRow
+                part="Keyboard 2"
+                show="We Will Rock You"
+                location="Leicester, England"
+                date="Mar 2025"
+              />
+              <ExperienceTableRow
+                part="Keyboard 2"
+                show="Joseph"
+                location="Leicester, England"
+                date="Mar 2024"
+              />
+              <ExperienceTableRow
+                part="Keyboard 1"
+                show="Sister Act"
+                location="Leicester, England"
+                date="Mar 2020"
+              />
+              <ExperienceTableRow
+                part="Rhythm Guitar"
+                show="FAME!"
+                location="Leicester, England"
+                date="Feb 2018"
+              />
+            </tbody>
+          </table>
+        </section>
+        <section>
+          <h2 className="mb-8 text-2xl sm:text-4xl">How I Got Here</h2>
+          <p className="mb-4">
+            Over the last decade I've also undertaken studies in Double Bass
+            (grade 4), tenor trombone (grade 8), and choral singing. When not
+            playing in pit bands, I also perform on-stage in amateur musical
+            theatre.
+          </p>
+          <p className="mb-4">
+            The above instrument studies were funded by Awards for Young
+            Musicians (now Young Sounds UK), a charity supporting young people
+            from lower income backgrounds. Because of this organisation, I had
+            the opportunity to play tenor trombone for the National Orchestra
+            For All (NOFA), as well as being accepted into the National Youth
+            Orchestra (NYO), and nominated for the Royal Birmingham
+            Conservetoire's "LEAP" youth ensemble.
+          </p>
+          <p>
+            If anyone in a lower income situation is trying to support a young
+            musician in their care, I would highly recommend getting in touch
+            with Young Sounds to discuss what options are available. I was
+            nominated for an award by my high school music teacher, who was
+            incredibly supportive of my musical growth. To this day, I am very
+            grateful for the opportunities they provided me with.
+          </p>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default MusicPortfolio;
+
+type ExperienceTableRowProps = {
+  part: string;
+  show: string;
+  location: string;
+  date: string;
+};
+
+const ExperienceTableRow: React.FC<ExperienceTableRowProps> = ({
+  part,
+  show,
+  location,
+  date,
+}) => {
+  return (
+    <tr>
+      <td>{part}</td>
+      <td>{show}</td>
+      <td className="hidden md:table-cell">{location}</td>
+      <td className="hidden sm:table-cell">{date}</td>
+    </tr>
+  );
+};
